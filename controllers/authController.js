dbcf5a18383a7f4676dfd08fce597834e01e5210
Zeddit <dchetveriakov@gmail.com>
2026-03-26 17:37:12 +0100

    feat(auth): add forgot and reset password routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import { User } from "../models/index.js";
 import jwt from "jsonwebtoken";
 import ApiError from "../utils/apiError.js";
+import crypto from "crypto";
+import { Op } from "sequelize";
 
 export const register = async (req, res) => {
     const { email, password } = req.body;
@@ -22,8 +24,8 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
 
     const existingUser = await User.scope("withPassword")
-                                .findOne({ where: { email: email } });
-    if(!existingUser) {
+        .findOne({ where: { email: email } });
+    if (!existingUser) {
         throw new ApiError("Incorrect email or password", 401);
     }
     const isMatch = await existingUser.correctPassword(password, existingUser.password_hash);
@@ -37,7 +39,7 @@ export const login = async (req, res) => {
         process.env.JWT_SECRET_KEY,
         { expiresIn: '1d' }
     );
-    
+
     existingUser.last_login = new Date();
     await existingUser.save();
 
@@ -45,5 +47,69 @@ export const login = async (req, res) => {
         status: "success",
         token,
         user: { id: existingUser.id, email: existingUser.email }
+    });
+};
+
+export const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+
+    const existringUser = await User.findOne({
+        where: { email: email }
+    });
+
+    if (!existringUser) {
+        return res.status(200).json({
+            status: "success",
+            message: "Reset link sent"
+        })
+    }
+
+    const resetToken = crypto.randomBytes(32)
+        .toString('hex');
+    const hashedToken = crypto.createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    await existringUser.update({
+        reset_password_token: hashedToken,
+        reset_password_expires: Date.now() + 15 * 60 * 1000
+    });
+
+    console.log(`Reset link: http://localhost:3000/auth/reset-password/${resetToken}`);
+
+    res.status(200).json({
+        status: "success",
+        message: "Reset link sent"
+    });
+};
+
+export const resetPassword = async (req, res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const hashedToken = crypto.createHash('sha256')
+        .update(token)
+        .digest('hex');
+
+    const existingUser = await User.scope('withPassword').findOne({
+        where: {
+            reset_password_token: hashedToken,
+            reset_password_expires: { [Op.gt]: new Date() }
+        }
+    });
+
+    if (!existingUser) {
+        throw new ApiError("Token is invalid or expired", 401);
+    }
+
+    existingUser.password_hash = password;
+    existingUser.reset_password_token = null;
+    existingUser.reset_password_expires = null;
+
+    await existingUser.save();
+    
+    res.status(200).json({
+        status: "success",
+        message: "Password reset successful"
     });
 };
